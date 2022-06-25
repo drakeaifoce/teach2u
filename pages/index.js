5bf80ad570ab627b58ac2bf66b480a89f59bad3d
drakeaifoce <drakeaifoce@gmail.com>
2022-06-25 19:57:29 +0600

    init tailwind css, daisy ui, tailwind typography

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Footer from '../components/footer/Footer'
+import { Navbar } from '../components/navbar/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar/>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["bumblebee"]
+  }
+}
