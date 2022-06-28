7716f0d58a9a76096aac64fcd706d42da424fc74
drakeaifoce <drakeaifoce@gmail.com>
2022-06-29 02:44:59 +0600

    feat: add styles, remember me, inputs and button in form; add rating to teacher card

--- components/modal/AuthForm.jsx
+++ components/modal/AuthForm.jsx
@@ -6,16 +6,21 @@ export function AuthForm() {
         <label for="my-modal-4" className="modal cursor-pointer">
             <label className="modal-box relative" for="">
             <label for="my-modal-4" className="btn btn-primary btn-sm btn-circle absolute right-2 top-2">✕</label>
-                <h3 className="text-lg font-bold md-auto">Войти</h3>
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">Email</span>
+                <div className="form-control w-full max-w-xs mx-auto">
+                    <h3 className="text-lg font-bold md-auto ">Войти</h3>
+                    <label className="label">
+                        <span className="label-text">Email</span>
                     </label>
-                    <input type="text" placeholder="email@mail.com" class="input input-bordered w-full max-w-xs focu" />
-                    <label class="label">
-                        <span class="label-text">Пароль</span>
+                    <input type="text" placeholder="email@mail.com" className="input input-bordered w-full max-w-xs focu" />
+                    <label className="label">
+                        <span className="label-text">Пароль</span>
                     </label>
-                    <input type="text" placeholder="email@mail.com" class="input input-bordered w-full max-w-xs focu" />
+                    <input type="text" placeholder="email@mail.com" className="input input-bordered w-full max-w-xs focu" />
+                    <label className="label cursor-pointer">
+                        <span className="label-text">Remember me</span> 
+                        <input type="checkbox"  className="checkbox" />
+                    </label>
+                    <a className="link link-hover text-">Нет аккаунта? Зарегистрируйтесь</a>
                 </div>
             </label>
         </label>
--- components/teachers/TeacherCard.jsx
+++ components/teachers/TeacherCard.jsx
@@ -2,13 +2,21 @@
 export function TeacherCard() {
     return (
         <div className="card w-4/5 bg-base-100 border mx-auto">
-            <figure>
-                <img className="mask mask-cirle" src='https://static.vecteezy.com/system/resources/previews/000/899/311/large_2x/teacher-portrait-photo.jpg' alt="Teacher"/>
+            <figure className="mask mask-circle">
+                <img className="" src='https://static.vecteezy.com/system/resources/previews/000/899/311/large_2x/teacher-portrait-photo.jpg' alt="Teacher"/>
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">Математика</h2>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Выбрать</button>
+                <div class="rating">
+                    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" disabled/>
+                    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" disabled />
+                    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" disabled />
+                    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" disabled />
+                    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" disabled />
+                </div>
+                <p>Lorem ipsum dolor sitamet, stphen hawkin so adipisicing elit. Ratione disuja doloremque reiciendi nemo.</p>
+                <div className="card-actions w-full">
+                    <button className="btn btn-primary w-full">Подробнее</button>
                 </div>
             </div>
         </div>      
