44d84f16d4d6554d609b487a4d5cb8b22dfb662e
drakeaifoce <drakeaifoce@gmail.com>
2022-06-30 21:42:02 +0600

    feat: implement debounce, code cleanup

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -29,7 +29,7 @@ export function Footer() {
                     </label> 
                     <div className="relative">
                         <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
-                        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Подписаться</button>
                     </div>
                 </div>
             </div>
